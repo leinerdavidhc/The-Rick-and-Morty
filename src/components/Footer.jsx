82843bbc8a755bc2db5236150c8fa7eb8e48e007
Leiner Hoyos Cabrera <107592704+leinerdavidhc@users.noreply.github.com>
2023-09-09 17:26:41 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-gray-800 p-4 text-white text-center">
-      <p>&copy; The Rick and Morty. Todos los derechos reservados.</p>
+      <p>&copy; Leiner Hoyos. Todos los derechos reservados.</p>
     </footer>
   );
 };
